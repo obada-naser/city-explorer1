aa3a4aaa9fcb4a1c781109f0378abf5e94f52c1f
obada-naser <obadanaser135@gmail.com>
2021-08-04 01:56:44 +0300

    trying to finish the front end problems

---
+++ src/components/latLon.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class LatLon extends React.Component {
+  render() {
+    return(
+      <>
+        <h2>Welcome to {this.props.city}</h2>
+        <p>{this.props.city} is located at {this.props.lat} by {this.props.lon}</p>
+      </>
+    )
+  }
+}
+
+export default LatLon;
+© 2021 GitHub, Inc.

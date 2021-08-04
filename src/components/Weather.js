e72e21e99b1d83c752e76186ceacdb7e35395ffd
obada-naser <obadanaser135@gmail.com>
2021-08-05 02:32:33 +0300

    solving the lab

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,11 +3,11 @@ import React from 'react';
 class Weather extends React.Component {
   render() {
     return(
-      this.props.weather.map((day, index) => (
-        <div key={index}>
+      this.props.weather.map((day) => (
+        <>
           <p>day: {day.date}</p>
           <p>description: {day.description}</p>
-        </div>
+      </>
       ))
     )
   }
